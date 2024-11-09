4b14369aec51625e57c70580b282db2a7ed08d93
MOHAMMED NAFIH YUSEPH <nafihpp@gmail.com>
2024-11-09 18:56:00 +0400

    Create tensorflow.ts

---
+++ tensorflow.ts
@@ -0,0 +1,31 @@
+import * as tf from '@tensorflow/tfjs';
+
+// Prepare data: y = 2x + 1
+const x_train = tf.tensor1d([1, 2, 3, 4, 5]);
+const y_train = tf.tensor1d([3, 5, 7, 9, 11]);
+
+// Create a simple model
+const model = tf.sequential();
+model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+// Compile the model
+model.compile({
+  optimizer: 'sgd',
+  loss: 'meanSquaredError',
+});
+
+// Train the model
+async function train() {
+  await model.fit(x_train, y_train, { epochs: 100 });
+  console.log('Model trained!');
+}
+
+// Make a prediction
+async function predict() {
+  const prediction = model.predict(tf.tensor1d([6])) as tf.Tensor;
+  prediction.print();  // Output: predicted value for x=6
+}
+
+train().then(() => {
+  predict();
+});

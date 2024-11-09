3ac2f5a8c4f10a3164521c6a466de136d3189d16
MOHAMMED NAFIH YUSEPH <nafihpp@gmail.com>
2024-11-09 19:11:21 +0400

    Update tensorflow.ts

--- tensorflow.ts
+++ tensorflow.ts
@@ -1,31 +1,31 @@
 import * as tf from '@tensorflow/tfjs';
 
-//Tensor: A multi-dimensional array. In this case, tensor1d means 1-dimensional.
+/** Tensor: A multi-dimensional array. In this case, tensor1d means 1-dimensional. **/
 
-// This is the input data (independent variable)
+/** This is the input data (independent variable) **/
 const x_train = tf.tensor1d([1, 2, 3, 4, 5]);
-//This is the target/output data (dependent variable)
+/** This is the target/output data (dependent variable) **/
 const y_train = tf.tensor1d([3, 5, 7, 9, 11]);
 
 
 
-// Create a simple model
+/** Create a simple model **/
 const model = tf.sequential();
 model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 
-// Compile the model
+/** Compile the model **/
 model.compile({
   optimizer: 'sgd',
   loss: 'meanSquaredError',
 });
 
-// Train the model
+/** Train the model **/
 async function train() {
   await model.fit(x_train, y_train, { epochs: 100 });
   console.log('Model trained!');
 }
 
-// Make a prediction
+/** Make a prediction **/
 async function predict() {
   const prediction = model.predict(tf.tensor1d([6])) as tf.Tensor;
   prediction.print();  // Output: predicted value for x=6
